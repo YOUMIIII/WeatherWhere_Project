7acea7b751d86b6e27568a165551cf1d9272c066
YOUMIIII <youmi8255@naver.com>
2023-03-04 23:20:25 +0900

    commit

---
+++ src/test/java/weatherwhere/team/TeamApplicationTests.java
@@ -0,0 +1,13 @@
+package weatherwhere.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
